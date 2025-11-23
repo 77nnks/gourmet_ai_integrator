8f664e8707982c9683730a12a26e74cd9d951e26
77nnks <77nnks@gmail.com>
2025-11-23 16:42:36 +0900

    Fix incorrect imports: move search_candidates and get_place_details to google_api module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,20 +1,26 @@
 # modules/__init__.py
 
-from .google_api import search_candidates, get_place_details
-from .ai_processing import (
+# --- Google API ---
+from .google_api import (
     search_candidates,
     get_place_details,
+)
+
+# --- AI Processing ---
+from .ai_processing import (
     summarize_reviews,
     infer_store_type,
     infer_recommendation,
     classify_tags,
 )
 
+# --- Notion 連携 ---
 from .notion_client import (
     upsert_store,
     build_page_url,
 )
 
+# --- Utils ---
 from .utils import (
     build_photo_url,
     TYPE_ICON,
@@ -27,21 +33,3 @@ from .utils import (
     trim_text,
     parse_location_query
 )
-
-__all__ = [
-    "search_candidates",
-    "get_place_details",
-    "summarize_reviews",
-    "infer_store_type",
-    "infer_recommendation",
-    "classify_tags",
-    "upsert_store",
-    "find_page_by_place_id",
-    "build_page_url",
-    "convert_price_level",
-    "calc_distance",
-    "extract_number",
-    "extract_text_without_numbers",
-    "trim_text",
-    "parse_location_query",
-]
